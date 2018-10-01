f06ba804abf4e745c8fd045fd5406b6684964c7a
Aaron Parsons <aaron.parsons@live.ca>
2018-10-01 21:13:17 +0000

    fixed App component not calling MessageList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Message from './Message.jsx';
+import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
 
@@ -10,7 +10,7 @@ class App extends Component {
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <Message />
+        <MessageList />
         <ChatBar />
       </div>
     );
