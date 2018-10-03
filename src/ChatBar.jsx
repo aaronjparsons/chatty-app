ef1d76340e4c5de7644432f03b3cda8df27e00c5
Aaron Parsons <aaron.parsons@live.ca>
2018-10-03 22:30:37 +0000

    added emojis! clicking smiley in textbox opens emoji picker and emojis are added to the text box when clicked

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
+import EmojiPicker from 'emoji-picker-react';
+import JSEMOJI from 'emoji-js';
+import Smiley from '../public/images/smiley.png';
+ 
+//emoji set up
+let jsemoji = new JSEMOJI();
+// set the style to emojione (default - apple)
+jsemoji.img_set = 'emojione';
+// set the storage location for all emojis
+jsemoji.img_sets.emojione.path = 'https://cdn.jsdelivr.net/emojione/assets/3.0/png/32/';
+
 
 class ChatBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      emojiTrue: false,
+    }
     this.handleMessage = this.handleMessage.bind(this);
     this.handleUsername = this.handleUsername.bind(this);
+    this.emojiToggle = this.emojiToggle.bind(this);
   }
   handleMessage(e){
     if (e.keyCode === 13) {
@@ -21,19 +36,45 @@ class ChatBar extends Component {
    }
   }
 
+  emojiToggle() {
+    const emojiState = this.state.emojiTrue;
+    this.setState({emojiTrue: !emojiState});
+  }
+
+  emojiClick(emoji, data) {
+    let emojiPic = jsemoji.replace_colons(`:${data.name}:`);
+    document.querySelector('.chatbar-message').value += emojiPic;
+  }
+
+  createEmojiTable() {
+    if (this.state.emojiTrue) {
+      return (
+        <div id='emoji-picker'>
+          <EmojiPicker onEmojiClick={this.emojiClick}/>
+        </div>
+      )
+    }
+    return null;
+  }
+
   render() {
+    const emojiTable = this.createEmojiTable();
     return (
-      <footer className='chatbar'>
-        <input className="chatbar-username" 
-          placeholder="Your Name (Optional)" 
-          onKeyDown={this.handleUsername} 
-          defaultValue={this.props.currentUser} 
-        />
-        <input className="chatbar-message" 
-          placeholder="Type a message and hit ENTER" 
-          onKeyDown={this.handleMessage}
-        />
-      </footer>
+      <div>
+        <footer className='chatbar'>
+          <input className="chatbar-username" 
+            placeholder="Your Name (Optional)" 
+            onKeyDown={this.handleUsername} 
+            defaultValue={this.props.currentUser} 
+          />
+          <input className="chatbar-message" 
+            placeholder="Type a message and hit ENTER" 
+            onKeyDown={this.handleMessage}
+          />
+        </footer>
+        <img id='emoji-toggle' src={Smiley} onClick={this.emojiToggle} />
+        {emojiTable}
+      </div>
     );
   }
 }
