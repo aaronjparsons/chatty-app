ab60d18109c489604fb00189e46f40f00cd71c1b
Aaron Parsons <aaron.parsons@live.ca>
2018-10-04 18:38:42 +0000

    modified userlist container

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -4,6 +4,7 @@ function UserList(props) {
     const users = props.userList.map((user) => {
       return (<p key={user} className='user-list'>{user}</p>);
     });
+
     return (
       <div>
         <div className='user-list-header'>{props.userCount} User(s) Online</div>
