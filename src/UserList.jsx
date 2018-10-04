bae0346597de6f591337f3bbe989c8a6f4af7072
Aaron Parsons <aaron.parsons@live.ca>
2018-10-04 15:54:51 +0000

    added text-overflow ellipsis to the username and userlist areas to help with long usernames

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class UserList extends Component {
   render() {
     const users = this.props.userList.map((user) => {
-      return (<p key={user}>{user}</p>);
+      return (<p key={user} className='user-list'>{user}</p>);
     });
     return (
       <div>
